19bf2d7499fb1dfeb1aa2666a515356463ee6538
anyhum <anyhum@yandex.ru>
2023-09-30 19:12:28 +0500

    OKX

--- RoverBot/Helpers/Exchange.cs
+++ RoverBot/Helpers/Exchange.cs
@@ -1,9 +1,59 @@
 ﻿using System;
 
+using OKX.Api;
+
 namespace RoverBot
 {
 	public static class Exchange
 	{
-		
+		public const string ApiKey = "0c3d85cc-bdf9-4e69-b8f2-ecf24493ccd6";
+
+		public const string SecretKey = "A9D9708CD86F133CCFA6AD8D5998AAC9";
+
+		public const string PassPhrase = "FTY19-641TD-331Eq";
+
+		private static readonly OKXRestApiClient Client;
+
+		static Exchange()
+		{
+			try
+			{
+				Client = new OKXRestApiClient();
+
+				Client.SetApiCredentials(ApiKey, SecretKey, PassPhrase);
+			}
+			catch(Exception exception)
+			{
+				Logger.Write("Exchange: " + exception.Message);
+			}
+		}
+
+		public static bool GetBalance(out decimal balance)
+		{
+			balance = default;
+
+			try
+			{
+				var task = Client.TradingAccount.GetAccountBalanceAsync();
+
+				task.Wait();
+
+				if(task.Result.Success)
+				{
+					
+					return true;
+				}
+				
+				Logger.Write("GetBalance: " + task.Result.Error.Message);
+
+				return false;
+			}
+			catch(Exception exception)
+			{
+				Logger.Write("GetBalance: " + exception.Message);
+
+				return false;
+			}
+		}
 	}
 }
--- RoverBot/Program.cs
+++ RoverBot/Program.cs
@@ -7,11 +7,9 @@ namespace RoverBot
 	{
 		public const string CheckLine = "******************************************************************************";
 
-		public const string Version = "2.02";
+		public const string Version = "2.05";
 		
 		public const string ApiKey = "0c3d85cc-bdf9-4e69-b8f2-ecf24493ccd6";
-
-		public const string SecretKey = "A9D9708CD86F133CCFA6AD8D5998AAC9";
 
 		public static void Main()
 		{
@@ -21,7 +19,12 @@ namespace RoverBot
 
 			Logger.Write(ApiKey);
 
-			
+			Exchange.GetBalance(out var balance);
+
+			while(true)
+			{
+				Thread.Sleep(1000);
+			}
 		}
 	}
 }
